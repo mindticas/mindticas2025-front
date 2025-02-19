b0ecdd9843c6079913c981b9d913d602e0d1e320
Jorge-Antonio-Torres-Lozoya <jtorreslozoya02@gmail.com>
2025-02-19 14:56:07 -0600

    fix: solve context issue in tests by wrapping components with BookingProvider

--- utils/render-test.tsx
+++ utils/render-test.tsx
@@ -1,10 +1,13 @@
 import { Provider } from '@/components/ui/provider';
+import { BookingProvider } from '@/context/BookingContext';
 import { render as rtlRender } from '@testing-library/react';
 
 export function render(ui: React.ReactNode) {
     return rtlRender(<>{ui}</>, {
         wrapper: (props: React.PropsWithChildren) => (
-            <Provider>{props.children}</Provider>
+            <Provider>
+                <BookingProvider>{props.children}</BookingProvider>
+            </Provider>
         ),
     });
 }
