fe4cb1e729de1d80d88521f16bdce97a72ae8810
Hiram-Devv <hiramdev02@gmail.com>
2025-02-25 12:00:21 -0600

    feature: calls to the back end were created

---
+++ pages/api/AppointmentService.ts
@@ -0,0 +1,79 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const createAppointment = async (newAppointment: any) => {
+    try {
+        const response = await fetch(`${API_URL}/appointments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newAppointment),
+        });
+        if (!response.ok) {
+            throw new Error('Error al enviar la cita');
+        }
+        return await response.json() // returns the response in json
+}   catch(error){
+    console.error('Error:', error)
+    throw error;
+}}
+// Get appointments
+export const getAppointment = async() => {
+    try {
+        const response = await fetch(`${API_URL}/appointments`)
+        console.log(response.json())
+        if(!response.ok){
+            throw new Error('Error al obtener las citas')
+    }
+        return await response.json()
+    } catch (error) {
+        console.log('Error', error)
+        throw error
+    }
+}
+// Delete appointments
+export const deleteAppointment = async (appointmentId : string) => {
+    try {
+        const response = await fetch(`${API_URL}/appointments/${appointmentId}`, {
+            method: 'DELETE',
+        })
+        if(!response.ok){
+            throw new Error('Error al eliminar la cita')
+        }
+        return await response.json() // Return the delete confirmation
+    } catch (error) {
+        console.log('Error', error)
+        throw error;
+    }
+}
+// Update Appointment
+export const updateAppointment = async(appointmentId : string, updatedData: any) => {
+    try {
+        const response = await fetch(`${API_URL}/appointments/${appointmentId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedData),
+        })
+        if(!response.ok){
+            throw new Error('Error al actualizar la cita')
+        }
+    } catch (error) {
+        console.log('Error', error)
+        throw error;
+    }
+}
+// Get the services
+export const getService = async() => {
+    try {
+        const response = await fetch(`${API_URL}/services`)
+        if(!response.ok){
+            throw new Error('Error al obtener los servicios')
+        }
+        return await response.json()
+    } catch (error) {
+        console.log('Error', error)
+        throw error
+    }
+}
