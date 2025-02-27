84c4dc651367a3b2cc5b58b45cdccc475eaf84df
Hiram-Devv <hiramdev02@gmail.com>
2025-02-27 09:47:22 -0600

    feature: the connection to the back end was created

--- pages/api/AppointmentService.ts
+++ pages/api/AppointmentService.ts
@@ -2,7 +2,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const createAppointment = async (newAppointment: any) => {
     try {
-        const response = await fetch(`${API_URL}/appointments`, {
+        const response = await fetch(`${API_URL}/appointment`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -14,17 +14,16 @@ export const createAppointment = async (newAppointment: any) => {
         }
         return await response.json() // returns the response in json
 }   catch(error){
-    console.error('Error:', error)
+    console.log('Error:', error)
     throw error;
 }}
 // Get appointments
 export const getAppointment = async() => {
     try {
-        const response = await fetch(`${API_URL}/appointments`)
-        console.log(response.json())
+        const response = await fetch(`${API_URL}/appointment`)
         if(!response.ok){
             throw new Error('Error al obtener las citas')
-    }
+    } 
         return await response.json()
     } catch (error) {
         console.log('Error', error)
@@ -34,7 +33,7 @@ export const getAppointment = async() => {
 // Delete appointments
 export const deleteAppointment = async (appointmentId : string) => {
     try {
-        const response = await fetch(`${API_URL}/appointments/${appointmentId}`, {
+        const response = await fetch(`${API_URL}/appointment/${appointmentId}`, {
             method: 'DELETE',
         })
         if(!response.ok){
@@ -49,7 +48,7 @@ export const deleteAppointment = async (appointmentId : string) => {
 // Update Appointment
 export const updateAppointment = async(appointmentId : string, updatedData: any) => {
     try {
-        const response = await fetch(`${API_URL}/appointments/${appointmentId}`, {
+        const response = await fetch(`${API_URL}/appointment/${appointmentId}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -67,7 +66,7 @@ export const updateAppointment = async(appointmentId : string, updatedData: any)
 // Get the services
 export const getService = async() => {
     try {
-        const response = await fetch(`${API_URL}/services`)
+        const response = await fetch(`${API_URL}/treatment`)
         if(!response.ok){
             throw new Error('Error al obtener los servicios')
         }
