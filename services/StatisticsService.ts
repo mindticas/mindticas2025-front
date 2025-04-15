4dec0319e0325a84d2b535f8d7acfbed00fd41a0
Hiram-Devv <hiramdev02@gmail.com>
2025-04-15 17:06:03 -0600

    temporal to work at home

---
+++ services/StatisticsService.ts
@@ -0,0 +1,35 @@
+import { StatisticsData } from "@/interfaces/statistics/StatisticsData";
+import { StatisticsDataResponse } from "@/interfaces/statistics/StatisticsDataResponse";
+import { API_URL } from './apiConfig';
+import Cookies from 'js-cookie';
+
+export const getStatistics = async (params: StatisticsData): Promise<StatisticsDataResponse> => {
+    try {
+        // Get token
+        const token = Cookies.get('AUTH_TOKEN')
+        if(!token){
+            throw new Error('No hay token de autenticacion disponible')
+        }
+        // Consultar la url con los parametros
+        let url = `${API_URL}/statistic?startDate=${params.startDate}&endDate=${params.endDate}`
+        // add treatment if exist
+        if(params.treatment){
+            url+= `&treatment=${params.treatment}`
+        }
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        if(!response.ok){
+            throw new Error('Error al obtener las estadísticas')
+        }
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error('Error fetching statistics:', error);
+        throw error; // Rethrow the error to ensure the function always returns a value or throws
+    }
+}
